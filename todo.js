a5d89b9ce956dd273e1137a1ebba9c6374adc99e
Ryan Silva <ryandotsilva@gmail.com>
2019-04-04 17:01:16 -0400

    Refactor displayTodos method

--- todo.js
+++ todo.js
@@ -73,9 +73,8 @@ var view = {
     displayTodos: function() {
         var todosUl = document.querySelector('ul');
         todosUl.innerHTML = '';
-        for (var i = 0; i < todoList.todos.length; i++) {
+        todoList.todos.forEach(function(todo, position) {
             var todoLi = document.createElement('li');
-            var todo = todoList.todos[i];
             var todoTextWithCompletion = '';
 
             if (todo.completed === true) {
@@ -84,11 +83,11 @@ var view = {
                 todoTextWithCompletion = '( ) ' + todo.todoText;
             }
 
-            todoLi.id = i;
+            todoLi.id = position;
             todoLi.textContent = todoTextWithCompletion;
             todoLi.appendChild(this.createDeleteButton());
             todosUl.appendChild(todoLi);
-        }
+        }, this);
     },
     createDeleteButton: function() {
         var deleteButton = document.createElement('button');
